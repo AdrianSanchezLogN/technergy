f1ab30084b7393e41cd2d99e589575843ea9aceb
AdrianSanchezLogN <adriansanchez.logn@gmail.com>
2014-12-26 10:02:30 -0600

    Mail Pass

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
 * @description Module and archives used by the server
-* @author Adrián Sánchez <sesamaua@gmail.com>
+* @author Adrián Sánchez <asanchez@technergycr.com>
 */
 
 var template_engine = 'dust';
@@ -10,9 +10,21 @@ var express = require('express'),
     cronJob = require('cron').CronJob,
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    cookieSession = require('cookie-session');
+    cookieSession = require('cookie-session'),
+	nodemailer = require('nodemailer');
  
 var app = express();
+
+var transporter  = nodemailer.createTransport({
+   service: 'Gmail',  // sets automatically host, port and connection security settings
+   auth: {
+       user: 'adriansanchez.logn@gmail.com',
+       pass: 'Technergy14AMSS'
+   },
+   tls: {
+       rejectUnauthorized: false
+   }
+});
  
 //app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
@@ -21,15 +33,7 @@ var app = express();
     //app.use('/admin', express.static(__dirname + '/admin/app'));
     app.use(express.static(__dirname));
 //});
-/*
-try {
-    var configJSON = fs.readFileSync(__dirname + "/config.json");
-    var config = JSON.parse(configJSON.toString());
-} catch(e) {
-    console.error("File config.json not found or is invalid: " + e.message);
-    process.exit(1);
-}
-paypal.init(config);*/
+
 
 if ( template_engine == 'dust' ) {
     var dust = require('dustjs-linkedin'),
@@ -64,336 +68,26 @@ var logger = new (winston.Logger)({
 });
 
 module.exports.logger = logger;
-/**
-* @description Login/out functions of the user
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description  Validate and give a session_token to the admin login
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// returns {Int} session_token
-//app.post('/admin/', login.adminlogIn);
-
-// ------------------------------------------------
-// description  Logs out and deletes the session_token of the admin login
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-//app.delete('/admin/', login.logOut);
-
-// ------------------------------------------------
-// description  Validate and give a session_token to the user login
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// returns {Int} session_token
-//app.post('/login/', login.logIn);
-
-// ------------------------------------------------
-// description  Logs out and deletes the session_token of the user login
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-//app.delete('/login/', login.logOut);
-
-
-/**
-* @description REST functions of the user
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description  Gets information from one or all the users
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the user
-// All the users http://localhost:9000/user/
-// A single user http://localhost:9000/user/?id=1
-//app.get('/user/', user.findUser);
-
-// ------------------------------------------------
-// description Insert a new user
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the user
-// http://localhost:9000/user/ + JSON
-//app.post('/user/', user.registerUser);
-
-// ------------------------------------------------
-// description Update a user
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the user
-// http://localhost:9000/user/?id=2 + JSON
-//app.put('/user/', user.updateUser); 
-
-// ------------------------------------------------
-// description Delete a user
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/user/?id=2
-//app.delete('/user/', user.deleteUser);
-
-
-/**
-* @description REST functions of the products
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description Search the products information that matches some criteria
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id, category, material, crafstman - {string} keys 
-// returns {JSON} information related to the product
-// All the products http://localhost:9000/product/
-// A single product http://localhost:9000/product/?id=1
-// All the products from a category http://localhost:9000/product/?cate=1
-// All the products from a craftsman http://localhost:9000/product/?craft=1
-// All the products made of a material http://localhost:9000/product/?mate=1
-// All the products matching a keyword  http://localhost:9000/product/?key1=Handmade&key2=Wood
-//app.get('/product/', product.findProduct); 
-
-// ------------------------------------------------
-// description Insert a new product
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the product
-// http://localhost:9000/product/ + JSON
-//app.post('/product/', product.addProduct); 
-
-// ------------------------------------------------
-// description Update a product
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the product
-// http://localhost:9000/product/?id=2 + JSON
-//app.put('/product/', product.updateProduct);
-
-// ------------------------------------------------
-// description Add an image to a product
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the image path
-// http://localhost:9000/product/?id=2 + JSON
-//app.put('/productImage/', product.addImageProduct);
-
-// ------------------------------------------------
-// description Removes an image from a product
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the image path
-// http://localhost:9000/product/?id=2 + JSON
-//app.put('/productImageRemove/', product.removeImageProduct);
-
-// ------------------------------------------------
-// description Delete a product
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/product/?id=2
-//app.delete('/product/', product.deleteProduct);
-
-
-/**
-* @description REST functions of the craftsmen
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description  Gets information from one or all the craftsmen
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the craftsman
-// All the crafstmen http://localhost:9000/craftsman/
-// A single craftsman http://localhost:9000/craftsman/?id=1
-//app.get('/craftsman/', craftsman.findCraftsman);
-
-// ------------------------------------------------
-// description Insert a new craftsman
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the craftsman
-// http://localhost:9000/craftsman/ + JSON
-//app.post('/craftsman/', craftsman.addCraftsman);
-
-// ------------------------------------------------
-// description Update a craftsman
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the craftsman
-// http://localhost:9000/craftsman/?id=2 + JSON
-//app.put('/craftsman/', craftsman.updateCraftsman);
-
-// ------------------------------------------------
-// description Delete a craftsman
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/craftsman/?id=2
-//app.delete('/craftsman/', craftsman.deleteCraftsman);
 
 
 
-/**
-* @description REST functions of the communities
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
+app.post('/email', function (req, res) {
+    var fecha = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, ''),
+    mailOptions = {
+		to: 'asanchez@technergycr.com', // receiver
+		 subject: 'Technergy: Desde la pagina web - Fecha: ' + fecha, // subject
+		 text: 'Correo: ' + req.body['email'] + '. \n'+ 'Nombre: ' + req.body['name'] + '. \n' + 'Texto: ' + req.body['text'] // body
+		 };
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error){
+			console.log(error);
+		}else{
+			console.log('Message sent: ' + info.response);
+		}
+	});
+	console.log(req.body);
+});
 
-// ------------------------------------------------
-// description Gets information from one or all the communities
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the community
-// All the communities http://localhost:9000/community/
-// A single community http://localhost:9000/community/?id=1
-//app.get('/community/', community.findCommunity);
-
-// ------------------------------------------------
-// description Insert a new community
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the community
-// http://localhost:9000/community/ + JSON
-//app.post('/community/', community.addCommunity);
-
-// ------------------------------------------------
-// description Update a community
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the community
-// http://localhost:9000/community/?id=2 + JSON
-//app.put('/community/', community.updateCommunity);
-
-// ------------------------------------------------
-// description Delete a community
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/community/?id=2
-//app.delete('/community/', community.deleteCommunity);
-
-
-/**
-* @description REST functions of the categories
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description Gets information from one or all the categories
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the category
-// All the categories http://localhost:9000/category/
-// A single category http://localhost:9000/category/?id=1
-//app.get('/category/', category.findCategory);
-
-// ------------------------------------------------
-// description Insert a new category
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the category
-// http://localhost:9000/category/ + JSON
-//app.post('/category/', category.addCategory);
-
-// ------------------------------------------------
-// description Update a category
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the category
-// http://localhost:9000/category/?id=2 + JSON
-//app.put('/category/', category.updateCategory);
-
-// ------------------------------------------------
-// description Delete a category
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/category/?id=2
-//app.delete('/category/', category.deleteCategory);
-
-
-/**
-* @description REST functions of the materials
-* @author Adrián Sánchez <adriansanchez.logn@gmail.com>
-*/
-
-// ------------------------------------------------
-// description Gets information from one or all the materials
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the material
-// All the material http://localhost:9000/material/
-// A single material http://localhost:9000/material/?id=1
-//app.get('/material/', material.findMaterial);
-
-// ------------------------------------------------
-// description Insert a new material
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the material
-// http://localhost:9000/material/ + JSON
-//app.post('/material/', material.addMaterial);
-
-// ------------------------------------------------
-// description Update a material
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the material
-// http://localhost:9000/material/?id=2 + JSON
-//app.put('/material/', material.updateMaterial);
-
-// ------------------------------------------------
-// description Delete a material
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/material/?id=2
-//app.delete('/material/', material.deleteMaterial);
-
-
-// ------------------------------------------------
-// description Gets information from one or all the news
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// returns {JSON} information related to the news
-// All the news http://localhost:9000/news/
-// A single news http://localhost:9000/news/?id=1
-//app.get('/news/', news.findNews);
-
-// ------------------------------------------------
-// description Insert a new news
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {JSON} information 
-// returns {JSON} information related to the news
-// http://localhost:9000/news/ + JSON
-//app.post('/news/', news.addNews);
-
-// ------------------------------------------------
-// description Update a news
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int, JSON} id and the information 
-// returns {JSON} information related to the news
-// http://localhost:9000/news/?id=2 + JSON
-//app.put('/news/', news.updateNews);
-
-// ------------------------------------------------
-// description Delete news
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param {Int} id 
-// http://localhost:9000/news/?id=2
-//app.delete('/news/', news.deleteNews);
-
-// ------------------------------------------------
-// description Insert a new image
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param submit form
-// http://localhost:9000/category/ + JSON
-//app.post('/image/', image.addImage);
-
-
-
-// ------------------------------------------------
-// description Insert a new image
-// author Adrián Sánchez <adriansanchez.logn@gmail.com>
-// param submit form
-// http://localhost:9000/category/ + JSON
-/*app.post('/paypal/', paypal.makePayment);
-app.get('/orderExecute', paypal.orderExecute);
-
-app.get('/email/', mail.findEmail);
-
-app.post('/email/', mail.sendPass);
-*/
 
 
 app.get('*', function (req, res) {
